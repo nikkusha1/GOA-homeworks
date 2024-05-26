2a97e4a4550ff8fc4048e42b5806812c7624ef70
nikkusha1 <giorgadzenika045@gmail.com>
2024-05-26 21:09:38 +0400

    second commit

--- day 01/lesson1.py
+++ day 01/lesson1.py
@@ -1,4 +1,4 @@
 name="nika"
 age=15
 hight=1.65
-print("My name is", name, "and i am",age,"years old","i am hight",hight)
+print("My name is", name, "and i am",age,"years old","i am ",hight)
---
+++ level 3/homework/level3.py/homework.py
@@ -0,0 +1,36 @@
+name="nika"
+age=15
+hight=1.65
+print("My name is", name, "and i am",age,"years old","i am ",hight)
+
+
+
+
+print(10 + 50)
+
+print(120 - 100)
+
+print(15 * 2)
+
+print(10.2 + 5)
+
+num1= 10
+num2= 5
+print(num1 + num2)
+
+name= "nika"
+surname= "giorgadze"
+print(name + surname)
+
+
+print(2 ** 2)
+
+num1= 15
+num2= 3
+print(num1 // num2)
+
+
+
+str1= "foot"
+str2= "ball"
+print(str1 + str2)
