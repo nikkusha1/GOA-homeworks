498a12bb6f3e4dcb4320ca1d847c48a73c11cacc
nikkusha1 <giorgadzenika045@gmail.com>
2024-06-20 16:54:13 +0400

    second commit

---
+++ level 6/classwork.py
@@ -0,0 +1,9 @@
+name=input("your name is: ")
+print("name")
+
+
+
+name = "nika"
+lastname = "giorgadze"
+age = 15
+print(f"your name is {name}. your lastname is {lastname}. your age is {age}.")
---
+++ level 6/homework.py
@@ -0,0 +1,36 @@
+#task2
+
+name = input("enter your name: ")
+lastname = input("enter your lastname: ")
+age = int(input("enter your age: "))
+gmail =input("enter your gmail: ")
+
+
+#task3
+num1 = int(input("enter first number: "))
+num2 = int(input("enter second number: "))
+num3 = int(input("enter third number: "))
+
+print(num1 + num2 + num3)
+print(num1 - num2 - num3)
+print(num1 * num2 * num3)
+print(num1 / num2 / num3)
+
+#task4
+price = int(input("price is: "))
+quattity = int(input("quatity is: "))
+print(price * quattity)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
