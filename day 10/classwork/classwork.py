c6bf90cdcc9e8792bb29d2a2de3f0fab3e617f46
nikkusha1 <giorgadzenika045@gmail.com>
2024-07-17 16:20:11 +0400

    second commit

---
+++ day 10/classwork/classwork.py
@@ -0,0 +1,17 @@
+counter = 1
+while counter <= 100:
+    print(counter)
+    counter = counter +1
+
+
+counter = 1
+while counter <= 10:
+    print(counter)
+    counter += 2
+
+
+counter = 100
+while counter >= 1:
+    print(counter)
+    counter -= 1
+
---
+++ day 10/homework/homework.py
@@ -0,0 +1,60 @@
+#tas 2
+for i in range(1, 11):
+    print(i)
+    print("Hello world!")
+
+#task 3
+counter = 0
+for i in range(11):
+    counter = counter + 1
+    print(counter)
+
+#task 4
+for i in range(0, 1001, 100):
+    print(i)
+
+
+#task 5
+for i in range(100, -1, -1):
+    print(i)
+
+
+
+#task 6
+counter = 1
+while counter <11:
+    print(counter)
+    print("Hello world!")
+    counter = counter + 1
+
+
+#task 7
+counter = 0
+while counter <11:
+    counter = counter + 1
+    print(counter)
+
+
+#task 8
+counter = 0
+while counter < 1000:
+    print(counter)
+    counter = counter +100
+
+
+
+#task 9
+counter = 100
+while counter >= 0:
+    print(counter)
+    counter = counter - 1
+
+
+
+
+
+
+
+
+
+
