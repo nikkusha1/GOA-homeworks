eda29775871524c1724809ef98ad36e0be7b193a
nikkusha1 <giorgadzenika045@gmail.com>
2024-08-13 21:33:57 +0400

    second commit

---
+++ day 14/classwork/classwork.py
@@ -0,0 +1,27 @@
+numbers = [1, 2 , 3, 4, 5, 6, 7, 8, 9, 10]
+
+sum_second_and_last = numbers[1] + numbers[-1] 
+difference_second_last_and_first = numbers[-2] - numbers[0]  
+product_fifth_and_sixth = numbers[4] * numbers[5] 
+
+print(sum_second_and_last)
+print(difference_second_last_and_first)
+print(product_fifth_and_sixth)
+
+
+list = [10, 20, 30, 40, 50]
+print(list)
+
+list[2] = 300  
+list[3] = 400  
+print(list)
+
+
+listn = ["good", "is", "programming"]
+print(listn[2], listn[1] ,listn[0])
+
+listn = ["good", "is", "programming"]
+print(listn[-1], listn[-2], listn[-3])
+
+
+
---
+++ day 14/homework/homework.py
@@ -0,0 +1,93 @@
+#task 2
+list = [
+    "item0",  # index 0
+    "item1",  # index 1
+    "item2",  # index 2
+    "item3",  # index 3
+    "item4",  # index 4
+    "item5",  # index 5
+    "item6",  # index 6
+    "item7",  # index 7
+    "item8",  # index 8
+    "item9",  # index 9
+    "item10", # index 10
+    "item11", # index 11
+    "item12", # index 12
+    "item13", # index 13
+    "item14"  # index 14
+]
+
+print(list[0])
+print(list[1])
+print(list[2])
+print(list[3])
+print(list[4])
+print(list[5])
+print(list[6])
+print(list[7])
+print(list[8])
+print(list[9])
+print(list[10])
+print(list[11])
+print(list[12])
+print(list[13])
+print(list[14])
+
+#negative 
+print(list[-15])
+print(list[-14])
+print(list[-13])
+print(list[-12])
+print(list[-11])
+print(list[-10])
+print(list[-9])
+print(list[-8])
+print(list[-7])
+print(list[-6])
+print(list[-5])
+print(list[-4])
+print(list[-3])
+print(list[-2])
+print(list[-1])
+
+
+#task 3
+list = [
+    "item0",  # index 0
+    "item1",  # index 1
+    "item2",  # index 2
+    "item3",  # index 3
+    "item4",  # index 4
+    "item5",  # index 5
+    "item6",  # index 6
+    "item7",  # index 7
+    "item8",  # index 8
+    "item9",  # index 9
+    "item10", # index 10
+    "item11", # index 11
+    "item12", # index 12
+    "item13", # index 13
+    "item14"  # index 14
+]
+print(list[:3])
+print(list[3:6])
+print(list[6:9])
+print(list[9:12])
+print(list[:12])
+
+#task 4
+list = ["horse", "rabbit", "mouse", "wolf", "lion"]
+print(list)
+
+list[0] = "house"
+list[1] = "green"
+list[3] = "volleyball"
+list[4] = "piano"
+print(list)
+
+#task 5
+list = ["teacher", "school", "flower", "light", "book"]
+print(list)
+
+list[1:3] = ["GOA", "codding"]
+print(list)
