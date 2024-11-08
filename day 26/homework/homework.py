8fe369a03fb83c123982dbad9eecaffc946f2848
nikkusha1 <giorgadzenika045@gmail.com>
2024-11-08 20:30:33 +0400

    second commit

---
+++ day 26/homework/homework.py
@@ -0,0 +1,36 @@
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+for key in my_dict.keys():
+    print(key)
+
+
+
+my_dict = {'a': 1, 'b': 2, 'c': 3}
+for value in my_dict.values():
+    print(value)
+
+
+
+dict1 = {'a': 1, 'b': 2, 'c': 3}
+dict2 = {'h': 10, 'y': 20, 'q': 30}
+dict3 = {'name': 'nika', 'age': 25, 'city': 'kutaisi'}
+dict4 = {'apple': 5, 'banana': 10, 'ruspberry': 7}
+dict5 = {'dog': 'black', 'cat': 'yellow', 'bird': 'green'}
+
+
+dict1 = {'star1': 'red', 'star2': 'blue', 'star3': 'yellow'}
+print("The color of star1 is " + dict1["star1"] + ", but star2 shines brighter with its " + dict1["star2"] + " hue.")
+
+dict2 = {'actor': 'John', 'director': 'Sarah', 'genre': 'thriller'}
+print("The film, a " + dict2["genre"] + " movie, was directed by " + dict2["director"] + " and starred " + dict2["actor"] + ".")
+
+dict3 = {'pizza': 'cheese', 'burger': 'beef', 'pasta': 'tomato'}
+print("My favorite meal is " + dict3["pizza"] + " pizza, though sometimes I crave a " + dict3["burger"] + " burger.")
+
+
+
+
+
+
+
+
+
